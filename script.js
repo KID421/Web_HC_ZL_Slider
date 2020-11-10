eb173fc4cd46f53ff871592151b38c4db5d594a1
KID421 <suchild7@gmail.com>
2020-11-10 18:57:40 +0800

    按鈕點擊函式完成

--- script.js
+++ script.js
@@ -10,8 +10,13 @@ function btnNext() {
     index++;                                    // 編號遞增
 
     if (index == items.length) index = 0;       // 如果編號超出範圍 編號歸零
+}
+// 按鈕函式：上一張
+function btnPrev() {
+    index--;                                    // 編號遞減
 
-    console.log(index);
+    if (index == -1) index = items.length - 1;  // 如果編號超出範圍 編號等於 長度-1
 }
 
 next.onclick = btnNext;                         // 點擊函式
+prev.onclick = btnPrev;                         // 點擊函式
